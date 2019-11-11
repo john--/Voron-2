8a08d289fd0c96145348724f5053b21f79cf677b
John Jardine <john@gprime.net>
2019-11-10 19:12:32 -0500

    A few initial travis-ci tests

---
+++ travis_scripts/ci-build.py
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIR=${PWD}
+CACHE_DIR=${PWD}/travis_cache
+
+chmod +x ${BASE_DIR}/scripts/*.py
+
+ADMESH_DIR=${CACHE_DIR}/admesh-0.98.4
+mkdir -p ${CACHE_DIR}
+cd ${CACHE_DIR}
+if [ ! -d ${ADMESH_DIR} ]; then
+  echo "Admesh cache miss; fetching and building ..."
+  wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
+  tar -zxvf admesh-0.98.4.tar.gz
+  cd ${ADMESH_DIR}
+  ./configure
+  make
+  chmod +x admesh
+fi
+cd ${BASE_DIR}
+sudo ln -s ${ADMESH_DIR}/admesh /usr/bin/admesh
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # Compare branch against master
+  git remote set-branches --add origin master
+  git fetch
+  git diff --name-only --diff-filter=AMR origin/master | xargs -n 1 -I {} ${BASE_DIR}/scripts/validate-file.py ${BASE_DIR}/{}
+else
+  # Compare head against the branch to merge into (PR)
+  git diff --name-only --diff-filter=AMR -R HEAD origin/${TRAVIS_BRANCH} | xargs -n 1 -I {} ${BASE_DIR}/scripts/validate-file.py ${BASE_DIR}/{}
+fi
+
