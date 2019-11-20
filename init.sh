c90052cddb9eb5f9859a65107ddb6e97745bdc47
John Jardine <john@gprime.net>
2019-11-19 19:24:21 -0500

    Squashed '.github_scripts/' changes from 8bd44f2..88751c5
    88751c5 Update
    645f865 Update
    
    git-subtree-dir: .github_scripts
    git-subtree-split: 88751c539da48eae9bef95adcce2775f624a8027

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@
 
 ## Note: This file should rarely change. This pulls down the latest CI script and executes it 
 
-git status
+git subtree pull --prefix ./github_scripts https://github.com/john--/BuildScripts.git master --squash -m "update"
 
-git subtree pull --prefix ./github_scripts https://github.com/john--/BuildScripts.git master --squash
-
-chmod +x ${PWD}/.github_scripts/*.py
-${PWD}/.github_scripts/ci-build.sh
+sh ${PWD}/.github_scripts/ci-build.sh
